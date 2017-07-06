ad689111766b52533d0c0c1d15ee673840799cf2
miguelff <miguelff@github.com>
2017-07-06 19:05:14 +0200

    Create a new request for replication delay

---
+++ lib/freno/client/requests/replication_delay.rb
@@ -0,0 +1,21 @@
+require_relative '../request'
+
+module Freno
+  class Client
+    module Requests
+      class ReplicationDelay < Check
+
+        protected
+
+        def process_response(*)
+          response = super
+          response.body["Value"]
+        end
+
+        def verb
+          :get
+        end
+      end
+    end
+  end
+end
