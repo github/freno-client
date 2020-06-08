e0a9d42f7182ad2f5eb755827a5955e83428fd5c
Steve Richert <laserlemon@github.com>
2020-06-08 14:14:30 -0400

    Support Freno's p=low "low priority" flag
    See: https://github.com/github/freno/pull/91/files#diff-fbdc8cde28a71961e53c2e83e2483c9aR154

--- lib/freno/client/requests/check_read.rb
+++ lib/freno/client/requests/check_read.rb
@@ -8,16 +8,23 @@ module Freno
         def initialize(**kwargs)
           super
 
-          app        = kwargs.fetch(:app)
-          store_type = kwargs.fetch(:store_type)
-          store_name = kwargs.fetch(:store_name)
-          threshold  = kwargs.fetch(:threshold)
+          app          = kwargs.fetch(:app)
+          store_type   = kwargs.fetch(:store_type)
+          store_name   = kwargs.fetch(:store_name)
+          threshold    = kwargs.fetch(:threshold)
+          low_priority = kwargs.fetch(:low_priority, false)
 
           check do
             present app: app, store_type: store_type, store_name: store_name, threshold: threshold
           end
 
-          @path = "check-read/#{app}/#{store_type}/#{store_name}/#{threshold.to_f.round(3)}"
+          # A low priority check is handled slightly differently by Freno. If
+          # the p=low GET parameter is passed, the check will fail for any app
+          # with failed checks within the last second. This failure is returned
+          # quickly, without checking the underlying metric.
+          query_string = low_priority ? "?p=low" : ""
+
+          @path = "check-read/#{app}/#{store_type}/#{store_name}/#{threshold.to_f.round(3)}#{query_string}"
         end
       end
     end
